82bbdc8e53ef25ea2b8cf37e2aa45eeb9fa6d9b4
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2025-03-13 17:39:19 -0300

    feat: add chart

--- 01_steamlint/streamlint.py
+++ 01_steamlint/streamlint.py
@@ -1,2 +1,6 @@
-print('Hello world')
-print('Hello Clean Architecture!')
+import streamlit as st
+import pandas as pd
+
+df = pd.read_csv("01 Spotify.csv")
+df.set_index('Track', inplace=True)
+st.bar_chart(df[df['Stream'] > 1000000000]['Stream'])
