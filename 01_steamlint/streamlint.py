6f01083dbd32f6d2007efbb9614333dd8951bac4
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2025-03-14 14:00:47 -0300

    feat: add input selectbox

--- 01_steamlint/streamlint.py
+++ 01_steamlint/streamlint.py
@@ -1,6 +1,16 @@
 import streamlit as st
 import pandas as pd
 
+st.set_page_config(
+    layout='wide',
+    page_title='Spotify songs'
+)
+
 df = pd.read_csv("01 Spotify.csv")
 df.set_index('Track', inplace=True)
-st.bar_chart(df[df['Stream'] > 1000000000]['Stream'])
+artists = df['Artist'].value_counts().index
+artist = st.selectbox('Artista', artists)
+df_filterd = df[df['Artist'] == artist]
+
+st.bar_chart(df_filterd['Stream'])
+st.write(artist)
