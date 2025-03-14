596b6e35d6236da7d37fe2a5ff25f6b78ed2697c
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2025-03-14 14:20:28 -0300

    feat: add multselectbox

--- 01_steamlint/streamlint.py
+++ 01_steamlint/streamlint.py
@@ -3,14 +3,22 @@ import pandas as pd
 
 st.set_page_config(
     layout='wide',
-    page_title='Spotify songs'
+    page_title='Spotify Songs'
 )
 
 df = pd.read_csv("01 Spotify.csv")
 df.set_index('Track', inplace=True)
+
 artists = df['Artist'].value_counts().index
 artist = st.selectbox('Artista', artists)
-df_filterd = df[df['Artist'] == artist]
+df_filtered_artist = df[df['Artist'] == artist]
 
-st.bar_chart(df_filterd['Stream'])
+albuns = df_filtered_artist['Album'].value_counts().index
+album = st.selectbox('Album', albuns)
+df_filtered_album = df[df['Album'] == album]
+
+display = st.checkbox('Mostrar')
+if display:
+    st.bar_chart(df_filtered_album['Stream'])
+
 st.write(artist)
