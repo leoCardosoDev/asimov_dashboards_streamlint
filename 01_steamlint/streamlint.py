8ec4dc7ba84192f0298ec08c554436a0e3a37fc0
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2025-03-14 14:32:49 -0300

    feat: add layout

--- 01_steamlint/streamlint.py
+++ 01_steamlint/streamlint.py
@@ -10,15 +10,20 @@ df = pd.read_csv("01 Spotify.csv")
 df.set_index('Track', inplace=True)
 
 artists = df['Artist'].value_counts().index
-artist = st.selectbox('Artista', artists)
+artist = st.sidebar.selectbox('Artista', artists)
 df_filtered_artist = df[df['Artist'] == artist]
 
 albuns = df_filtered_artist['Album'].value_counts().index
 album = st.selectbox('Album', albuns)
 df_filtered_album = df[df['Album'] == album]
 
-display = st.checkbox('Mostrar')
-if display:
-    st.bar_chart(df_filtered_album['Stream'])
+#display = st.checkbox('Mostrar')
+#if display:
+#    st.bar_chart(df_filtered_album['Stream'])
 
+# col1, col2 = st.columns(2)
+col1, col2 = st.columns([0.7, 0.3])
+col1.bar_chart(df_filtered_album['Stream'])
+col2.line_chart(df_filtered_album['Danceability'])
 st.write(artist)
+st.sidebar.button('Teste')
