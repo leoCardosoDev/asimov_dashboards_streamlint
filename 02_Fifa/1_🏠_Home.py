3ea3f0eb55c2221eac3771416dc8199f28d76e99
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2025-03-15 18:30:02 -0300

    feat: apply player file

--- 02_Fifa/1_🏠_Home.py
+++ 02_Fifa/1_🏠_Home.py
@@ -3,6 +3,12 @@ import pandas as pd
 import webbrowser
 from datetime import datetime
 
+st.set_page_config(
+    page_title="Home",
+    page_icon="🏠",
+    layout="wide"
+)
+
 if "data" not in st.session_state:
     df_data = pd.read_csv("datasets/CLEAN_FIFA23_official_data.csv", index_col=0)
     df_data = df_data[df_data["Contract Valid Until"] >= datetime.today().year]
