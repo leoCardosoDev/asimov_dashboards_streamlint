ed1c8b046ada2f2ad565aab18a650ff426f44c6f
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2025-03-15 17:55:38 -0300

    feat: add club player

--- 02_Fifa/pages/2_⛹️_Player.py
+++ 02_Fifa/pages/2_⛹️_Player.py
@@ -1,1 +1,9 @@
 import streamlit as st
+
+df_data = st.session_state['data']
+clubes = df_data['Club'].value_counts().index
+club = st.sidebar.selectbox('Clube', clubes)
+
+df_players = df_data[(df_data['Club'] == club)]
+players = df_players['Name'].value_counts().index
+player = st.sidebar.selectbox('Jogador', players)
