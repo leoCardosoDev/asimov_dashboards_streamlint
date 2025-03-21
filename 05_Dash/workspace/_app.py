dfe583b8a9da2629d88967efcdaab37f69e46184
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2025-03-21 14:27:12 -0300

    chore: add model to app

---
+++ 05_Dash/workspace/_app.py
@@ -0,0 +1,11 @@
+import dash
+from dash import html, dcc
+import plotly.express as px
+import pandas as pd
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div([])
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8050)
